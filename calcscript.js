c63a14debfdb28d32796286ffb65a7d79977e3b5
Gabriel Tadeu <gabrieltadeums11@outlook.com>
2023-07-12 23:47:53 -0300

    Calculadora com html/css/javascript

---
+++ calcscript.js
@@ -0,0 +1,21 @@
+
+function insert(num){
+    let number = document.querySelector('.result').innerHTML;
+    document.querySelector('.result').innerHTML = number + num
+}
+
+function clean(){
+    document.querySelector('.result').innerHTML = ""
+}
+
+function back(){
+    let result = document.querySelector('.result').innerHTML;
+    document.querySelector('.result').innerHTML = result.substring(0, result.length -1)
+}
+
+function calculate(){
+    let result = document.querySelector('.result').innerHTML
+    if(result){
+        document.querySelector('.result').innerHTML = eval(result)
+    }
+  }
